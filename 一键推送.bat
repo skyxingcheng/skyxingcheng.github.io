10248a48c98c05223e91a91e72f8b80a5c80869e
skyxingcheng <skyxingcheng233@outlook.com>
2021-02-26 21:22:03 +0800

    20210226212203

--- 一键推送.bat
+++ 一键推送.bat
@@ -1,4 +1,6 @@
-git add -A
-git commit -m"%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
-git push -u origin master -f
+PowerShell -Command "& {git add *}"
+echo.
+PowerShell -Command "& {git commit -m"%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"}"
+echo.
+PowerShell -Command "& {git push}"
 pause
