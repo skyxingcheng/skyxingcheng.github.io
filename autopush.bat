9c3d1996769d1cb6a365ed4b5dc0a0c0c2333323
skyxingcheng <skyxingcheng233@outlook.com>
2021-02-27 22:12:12 +0800

    20210227221211

--- autopush.bat
+++ autopush.bat
@@ -1,3 +1,6 @@
+@echo off
+color 0a
+pushd %~dp0
 PowerShell -Command "& {git add *}"
 echo.
 PowerShell -Command "& {git commit -m"%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"}"
